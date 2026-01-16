af7dd444f93edbd2bc1f4fc586a7a2f96caf3c9a
Piyush Rai <56262183+piyushrai02@users.noreply.github.com>
2026-01-16 23:52:32 +0530

    Update demo.component.ts

--- src/app/demo.component.ts
+++ src/app/demo.component.ts
@@ -9,7 +9,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 })
 export class GreetingComponent implements OnInit {
   // Properties (data) that can be displayed or manipulated
-  title: string = "Welcome to Mycwdqw Angular 1 wqq sawq   wqqd";
+  title: string = "Welcome to Mycwdqw Angular 1 wqq sawq";
   userName: string = "Guest";
   initialUserName: string = "Guest"; // To store the initial state for reset
   showDetails: boolean = false;
