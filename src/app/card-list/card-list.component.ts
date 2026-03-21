e286eb604f9fd8d834941b96125d59a471c93491
Piyush Rai <56262183+piyushrai02@users.noreply.github.com>
2026-03-21 15:22:38 +0530

    Update card-list.component.ts

--- src/app/card-list/card-list.component.ts
+++ src/app/card-list/card-list.component.ts
@@ -11,7 +11,7 @@ export class CardListComponent implements OnInit {
   products: Product[] = [];
   paginatedProducts: Product[] = [];
 
-  // Pagination State
+  // Pagination State ds
   currentPage: number = 2;
   pageSize: number = 7;
   totalPages: number = 1;
